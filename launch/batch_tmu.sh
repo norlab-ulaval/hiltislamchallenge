620ff127a4a05e042f2625bcf0c6a4a0ac249d1f
Damien LaRocque <phicoltan@gmail.com>
2022-04-04 17:05:29 -0400

    Debug batch script

--- launch/batch_tmu.sh
+++ launch/batch_tmu.sh
@@ -1,7 +1,7 @@
 for file in $1/*.bag;
     do
     echo Processing $file;
-    stem=$(basename -- "$bagfile");
+    stem=$(basename -- "$file");
     ext="${stem##*.}";
     stem="${stem%.*}";
     roslaunch hiltislamchallenge hilti_tmu.launch path:=$1 bagname:=$stem;
