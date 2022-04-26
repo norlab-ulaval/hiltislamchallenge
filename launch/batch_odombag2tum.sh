4e2908f632e9bd60533a97caf3d63d4718464939
Damien LaRocque <phicoltan@gmail.com>
2022-04-26 12:02:53 -0400

    Batch process of both rosbag 2 tum and odom bag 2 tum

---
+++ launch/batch_odombag2tum.sh
@@ -0,0 +1,13 @@
+script_dir=$(dirname $(readlink -f $0))
+parent_dir="$(dirname "$script_dir")"
+
+for file in $1/*.bag;
+    do
+    echo Processing $file;
+    stem=$(basename -- "$file");
+    ext="${stem##*.}";
+    stem="${stem%.*}";
+
+    ${parent_dir}/tum_export/bag2tum.py -b $file
+    # roslaunch hiltislamchallenge hilti_tmu.launch path:=$1 bagname:=$stem;
+done
