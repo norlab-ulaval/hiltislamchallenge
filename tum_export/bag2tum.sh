e9f59465e9eb0deb2cc252d3a3f63d6025c696d3
Damien LaRocque <phicoltan@gmail.com>
2022-04-26 11:58:54 -0400

    Changing where we save odom bags

--- tum_export/bag2tum.sh
+++ tum_export/bag2tum.sh
@@ -9,23 +9,27 @@ stem="${stem%.*}";
 
 map_dir=$path/maps
 traj_dir=$path/trajectories
-odombag_path=$path/${stem}_odom.bag
+odom_dir=$path/odom
+odombag_path=${odom_dir}/${stem}_odom.bag
 script_dir=`dirname $0`
 
+mkdir -p $odom_dir
 mkdir -p $map_dir
 mkdir -p $traj_dir
 
 # roscore &
-roslaunch hiltislamchallenge deskewed_rosbag_to_tum.launch path:=$path bagname:=$stem
+# roslaunch hiltislamchallenge deskewed_rosbag_to_tum.launch path:=$path bagname:=$stem
+roslaunch hiltislamchallenge hilti_tmu.launch path:=$path bagname:=$stem
 
 echo "MAPS : saving to $map_dir"
-# rosservice call /save_trajectory "trajectory_file_name:
-#    data: '$map_dir/$2_traj.vtk'"
-# rosservice call /save_map "map_file_name:
-#    data: '$traj_dir/$2_map.vtk'"
+rosservice call /save_trajectory "trajectory_file_name:
+   data: '$map_dir/$2_traj.vtk'"
+rosservice call /save_map "map_file_name:
+   data: '$traj_dir/$2_map.vtk'"
 echo "MAPS : Exported"
 echo "----"
 echo "TUM : saving to $traj_dir"
+echo "TUM : Odom Bag path $odombag_path"
 ${script_dir}/bag2tum.py -b $odombag_path
 echo "TUM: Exported"
 
